79a271d5766fe91660d22128ce5b5230ef9e2b9e
Luigi Fiorelli <luigifiorelli91@live.it>
2014-06-26 16:49:01 +0200

    MOdifica di AutoGame per incorporare l'evento

--- src/giocatoreAutomatico/AutoGame.java
+++ src/giocatoreAutomatico/AutoGame.java
@@ -7,21 +7,24 @@
 package giocatoreAutomatico;
 
 import game2048.GameManager;
-import java.awt.AWTException;
-import java.awt.Robot;
+import giocatoreAutomatico.event.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.scene.input.KeyEvent;
+import javax.swing.event.EventListenerList;
 /**
  *
  * @author Luigi Fiorelli
  */
 public class AutoGame implements Runnable{
+    private final int TIME_DELAY_BOT = 2000; //ms tra una mossa e l'altra del bot
     private GameManager gM;
-    private static GiocatoreAutomatico gA;
-    private static Griglia gr;
+    private GiocatoreAutomatico gA;
+    private Griglia gr;
+    
+    private EventListenerList listeners;
     
     public AutoGame(GameManager gM){
+        listeners = new EventListenerList();
         this.gM=gM;
             try {
                 gA = GiocatoreAutomatico.getGiocatoreAutomatico();
@@ -29,17 +32,35 @@ public class AutoGame implements Runnable{
                 Logger.getLogger(AutoGame.class.getName()).log(Level.SEVERE, null, ex);
                }
             gr = new MyGriglia();
-        }
+    }
     
     @Override
     public void run() {
         while(true){
             int m = gA.prossimaMossa(gr);
             System.out.print(m);
-            //gM.move(m);
-            //gM.move(1);
-           
-            Attendi.ms(500);
+           fireNewDirection(m);
+                //gM.move(m);
+                //gM.move(1);
+                Attendi.ms(TIME_DELAY_BOT);
+
         }
+    }
+    
+    private void fireNewDirection(int move){
+        BotEvent event = new BotEvent(this, move);
+        Object[] listenersArray = listeners.getListenerList();
+        for(int i = listenersArray.length - 2; i >= 0; i -= 2){
+            if(listenersArray[i] == BotEventListener.class){
+                ((BotEventListener)listenersArray[i+1]).botNewDirectiondEvent(event);
+            }
+        }
+    }
+    public void addBotEventListener(BotEventListener l){
+        listeners.add(BotEventListener.class, l);
+    }
+
+    public void removeBotEventListener(BotEventListener l){
+        listeners.remove(BotEventListener.class, l);
     }
 }
