08931a5bfba23d6eddc3a88d9dab0cf765f8bb7c
Luigi Fiorelli <luigifiorelli91@live.it>
2014-06-26 11:37:50 +0200

    Aggiunta classe Attendi che permette di aspettare n millisecondi tramite il metodo statico ms Aggiunta classe AutoGame. E' un thread che chiede la mossa successiva alla classe GiocatoreAutomatico e richiama il metodo move di GameManager

--- src/giocatoreAutomatico/Attendi.java
+++ src/giocatoreAutomatico/Attendi.java
@@ -11,11 +11,20 @@ import java.util.logging.Logger;
  */
 public class Attendi {
 
+    /*
+     * Attendi n millisecondi
+    */
     public static void ms(int n){
         try {
             Thread.sleep(n);
         } catch (InterruptedException ex) {
             Logger.getLogger(Attendi.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+    /*
+     * Attendi n secondi
+    */
+    public static void s(int n){
+        ms(n*1000);
     }
 }
---
+++ src/giocatoreAutomatico/MyGriglia.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package giocatoreAutomatico;
+
+import game2048.Location;
+import giocatoreAutomatico.*;
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+/**
+ *
+ * @author Luigi
+ */
+public class MyGriglia implements Griglia{
+
+    @Override
+    public int size() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Integer get(Object key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Integer put(Location key, Integer value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Integer remove(Object key) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void putAll(Map<? extends Location, ? extends Integer> m) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Set<Location> keySet() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<Integer> values() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Set<Entry<Location, Integer>> entrySet() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    
+}
