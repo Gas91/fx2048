19fbf26b4778a6aa4e811e60f34edfa34434aa7e
Luigi Fiorelli <luigifiorelli91@live.it>
2014-06-26 17:05:48 +0200

    Movimenti dei tile tramite simulazione pulsanti tatiera

--- src/game2048/SimpleBotEventListener.java
+++ src/game2048/SimpleBotEventListener.java
@@ -1,8 +1,12 @@
 
 package game2048;
 
+
 import giocatoreAutomatico.event.BotEvent;
 import giocatoreAutomatico.event.BotEventListener;
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 
 /**
  *
@@ -18,20 +22,33 @@ public class SimpleBotEventListener implements BotEventListener{
     public void botNewDirectiondEvent(BotEvent e) {
         System.out.println("x");
         
-        switch(e.getResult()){
-            case 0:
-                gM.move(Direction.UP);
-            break;
-            case 1:
-                gM.move(Direction.RIGHT);
-            break;
-            case 2:
-                gM.move(Direction.LEFT);
-            break;
-            default:
-                gM.move(Direction.RIGHT);
-            break;
-        }
+        try {
+            Robot robot = new Robot();
+            switch(e.getResult()){
+                case 0:
+                    //gM.move(Direction.UP);
+                    robot.keyPress(KeyEvent.VK_UP);
+                    robot.keyRelease(KeyEvent.VK_UP);
+                break;
+                case 1:
+                    //gM.move(Direction.RIGHT);
+                    robot.keyPress(KeyEvent.VK_RIGHT);
+                    robot.keyRelease(KeyEvent.VK_RIGHT);
+                break;
+                case 2:
+                    //gM.move(Direction.LEFT);
+                    robot.keyPress(KeyEvent.VK_LEFT);
+                    robot.keyRelease(KeyEvent.VK_LEFT);
+                break;
+                default:
+                    //gM.move(Direction.DOWN);
+                    robot.keyPress(KeyEvent.VK_DOWN);
+                    robot.keyRelease(KeyEvent.VK_DOWN);
+                break;
+            }
+        } catch (AWTException e2) {
+            e2.printStackTrace();
+}
         
         //gM.move(e.getResult());
     }
