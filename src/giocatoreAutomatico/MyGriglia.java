01e9186f387e2cddd42b843b7a5434cc0edd3b93
Luigi Fiorelli <luigifiorelli91@live.it>
2014-07-01 22:33:39 +0200

    Implementazione di alcuni metodi e Javadoc

--- src/giocatoreAutomatico/MyGriglia.java
+++ src/giocatoreAutomatico/MyGriglia.java
@@ -1,20 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package giocatoreAutomatico;
 
 import game2048.Location;
-import giocatoreAutomatico.*;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 /**
- *
- * @author Luigi
+ * Implementazione della classe Griglia.
+ * Questa classe permette di creare una griglia con una Key di tipo Location e un valore di tipo Integer
+ * @author Luigi Fiorelli
  */
 public class MyGriglia implements Griglia{
     private final Map<Location, Integer> gameGrid;
@@ -23,26 +17,6 @@ public class MyGriglia implements Griglia{
         gameGrid = new HashMap<>();
     }
     
-    @Override
-    public int size() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean isEmpty() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean containsKey(Object key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean containsValue(Object value) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
     @Override
     public Integer get(Object key) {
         return gameGrid.get(key);
@@ -53,10 +27,30 @@ public class MyGriglia implements Griglia{
         gameGrid.put(key, value);
         return 1;
     }
+    
+    @Override
+    public int size() {
+        return gameGrid.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return gameGrid.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        return gameGrid.containsKey(key);
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        return gameGrid.containsValue(value);
+    }
 
     @Override
     public Integer remove(Object key) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return gameGrid.remove(key);
     }
 
     @Override
@@ -66,7 +60,7 @@ public class MyGriglia implements Griglia{
 
     @Override
     public void clear() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        gameGrid.clear();
     }
 
     @Override
@@ -83,6 +77,4 @@ public class MyGriglia implements Griglia{
     public Set<Entry<Location, Integer>> entrySet() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-
-    
 }
