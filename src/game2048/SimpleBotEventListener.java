24f01005230da2a6904700bda68c15ecad2fe038
Luigi Fiorelli <luigifiorelli91@live.it>
2014-07-01 17:15:06 +0200

    Inserita la parte di codice che richiama la mossa successiva

--- src/game2048/SimpleBotEventListener.java
+++ src/game2048/SimpleBotEventListener.java
@@ -2,55 +2,67 @@
 package game2048;
 
 
+import giocatoreAutomatico.Attendi;
+import giocatoreAutomatico.GiocatoreAutomatico;
 import giocatoreAutomatico.event.BotEvent;
 import giocatoreAutomatico.event.BotEventListener;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.scene.input.KeyCode;
 
 /**
  *
- * @author Luigi
+ * @author Luigi Fiorelli
  */
 public class SimpleBotEventListener implements BotEventListener{
     private GameManager gM;
+    private GiocatoreAutomatico gA;
     public SimpleBotEventListener(GameManager gM){
         this.gM = gM;
+        try {
+            gA = GiocatoreAutomatico.getGiocatoreAutomatico();
+        } catch (Exception ex) {
+            Logger.getLogger(SimpleBotEventListener.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     @Override
     public void botNewDirectiondEvent(BotEvent e) {
-        System.out.println("x");
-        
+        int m = gA.prossimaMossa(gM.getGriglia()); //Scelgo la mossa successiva
+        System.out.println("x"+m);
         try {
             Robot robot = new Robot();
-            switch(e.getResult()){
+            switch(m){
                 case 0:
                     //gM.move(Direction.UP);
                     robot.keyPress(KeyEvent.VK_UP);
+                    Attendi.ms(50);
                     robot.keyRelease(KeyEvent.VK_UP);
                 break;
                 case 1:
                     //gM.move(Direction.RIGHT);
                     robot.keyPress(KeyEvent.VK_RIGHT);
+                    Attendi.ms(50);
                     robot.keyRelease(KeyEvent.VK_RIGHT);
                 break;
                 case 2:
                     //gM.move(Direction.LEFT);
                     robot.keyPress(KeyEvent.VK_LEFT);
+                    Attendi.ms(50);
                     robot.keyRelease(KeyEvent.VK_LEFT);
                 break;
                 default:
                     //gM.move(Direction.valueFor(KeyCode.DOWN));
                     robot.keyPress(KeyEvent.VK_DOWN);
+                    Attendi.ms(50);
                     robot.keyRelease(KeyEvent.VK_DOWN);
                 break;
             }
         } catch (AWTException e2) {
             e2.printStackTrace();
         }
-        
-        //gM.move(e.getResult());
     }
 }
